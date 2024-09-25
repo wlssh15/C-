b37d61b4d8977929b54e6792c3305567b4403202
wlssh15 <jjjsh1215@naver.com>
2024-09-25 13:04:40 +0900

    Create 4주차(문자열실습과제1).c

---
+++ 4주차(문자열실습과제1).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	char message2[5] = { 'a','b','c','d','\0'};
+	char message[5] = {'a','b','c','\0'};
+	
+	printf("message=%s,message2=%s", message, message2);
+
+}
