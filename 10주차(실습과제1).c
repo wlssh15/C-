86f2c3e2d3a051278898088357ac4a63a0706a30
wlssh15 <jjjsh1215@naver.com>
2024-11-09 14:13:44 +0900

    Update 10주차(실습과제1).c

--- 10주차(실습과제1).c
+++ 10주차(실습과제1).c
@@ -20,3 +20,28 @@ int main()
 
 	return 0;
 }
+// #include<stdio.h>
+// #define SIZE 100
+
+// int main()
+// {
+// 	char input[SIZE];
+// 	FILE* fp = NULL;
+//     int i=0;
+// 	if ((fp = fopen("output.bin", "rb")) == NULL)
+// 	{
+// 		printf("error...");
+// 		return 0;
+// 	}
+//     while(!feof(fp))
+//     {
+//         fread(&input[i],sizeof(char),1,fp);
+//         i++;
+//     }
+//     input[--i]='\0';
+//     puts(input);
+    
+// 	fclose(fp);
+
+// 	return 0;
+// }
