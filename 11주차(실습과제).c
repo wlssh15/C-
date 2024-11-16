4fc93b7f88df4be6c2a6548afbba5c5acc09ca91
wlssh15 <jjjsh1215@naver.com>
2024-11-16 13:45:08 +0900

    Create 11주차(실습과제).c

---
+++ 11주차(실습과제).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char* pChar;
+	int* pInt;
+	int count;
+
+	scanf_s("%d", &count);
+	pInt = (int*)malloc(count * sizeof(int));
+
+	for (int i = 0; i < count; i++) pInt[i] = rand() % 100;
+	for (int i = 0; i < count; i++) printf(" %d ", pInt[i]);
+	pChar = (char*)pInt;
+	while (getchar() != '\n');
+	gets(pChar);
+	puts(pChar);
+
+	free(pInt);
+	return 0;
+}
