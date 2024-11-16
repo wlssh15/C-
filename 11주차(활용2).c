7475b7373e63cf5c07aad1dac509b793b2b4f3fe
wlssh15 <jjjsh1215@naver.com>
2024-11-16 15:19:26 +0900

    Create 11주차(활용2).c

---
+++ 11주차(활용2).c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct point {
+	int x;
+	int y;
+}POINT;
+int inputMenu() {
+	int input;
+	printf("메뉴입력(1.구조체 만들기,2.구조체 출력하기,-1.종료)");
+	scanf_s("%d", &input);
+	return input;
+}
+POINT* makePoint() {
+	POINT* p = (POINT*)malloc(sizeof(POINT));
+	p->x = rand() % 100;
+	p->y = rand() % 100;
+	return p;
+}
+void printPoint(POINT*p[],int size) {
+	for (int i = 0; i < size; i++)
+		printf("x:%d y:%d\n", p[i]->x, p[i]->y);
+}
+void freePoints(POINT* p[], int size) {
+	for (int i = 0; i < size; i++)
+		free(p[i]);
+}
+int main()
+{
+	int menu;
+	int count = 0;
+	POINT* points[100];
+	do {
+		menu = inputMenu();
+		printf("%d\n", menu);
+		if (menu == 1) {
+			//구조체 만들기
+			points[count] = makePoint();
+			count++;
+			printf("구조체를 만들었습니다.");
+		}
+		else if (menu == 2) {
+			//구조체 출력하기
+			printPoint(points, count);
+		}
+		else if (menu == -1) {
+			freePoints(points, count);
+			printf("프로그램 종료");
+		}
+	} while (menu > 0);
+
+	return 0;
+}
