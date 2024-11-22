6244095b89cec054fe096babf53e0590e7c0ba5b
wlssh15 <jjjsh1215@naver.com>
2024-11-22 17:18:34 +0900

    Create 12주차(시간처리).c

---
+++ 12주차(시간처리).c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<time.h>
+#include"report.h"
+
+
+int main(int argc,char*argv[])
+{
+	time_t current;
+	struct tm* timeinfo;
+
+	int year, month, day;
+	char* WEEK[] = {"sun","mon","tue","wed","thu","fri","sat"};
+
+	printf("Enter YYYY MM DD :");
+	scanf_s("%d %d %d", &year, &month, &day);
+
+	current = time(NULL);
+	timeinfo = localtime(&current);
+
+	timeinfo->tm_year = year-1900;
+	timeinfo->tm_mon = month - 1;
+	timeinfo->tm_mon = day;
+
+	mktime(timeinfo);
+
+	printf("%d %d %d->%s", year, month, day, WEEK[timeinfo->tm_wday]);
+
+
+}
